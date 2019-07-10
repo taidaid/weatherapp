5c181b724050f6982bd45c9ee088224db4caa7c8
Bryan <windsor.bryan@gmail.com>
2019-07-10 14:21:37 +0200

    added minor css to app-body

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,39 +5,39 @@ import Navbar from "../../components/Navbar/Navbar";
 import Location from "../../components/Location/Location";
 
 function App() {
-	const authenticate = () => {
-		return new Promise(resolve => setTimeout(resolve, 500));
-	};
+  const authenticate = () => {
+    return new Promise(resolve => setTimeout(resolve, 500));
+  };
 
-	useEffect(() => {
-		authenticate().then(() => {
-			const ele = document.getElementById("ipl-progress-indicator");
-			if (ele) {
-				// fade out
-				ele.classList.add("available");
-				setTimeout(() => {
-					// remove from DOM
-					ele.outerHTML = "";
-				}, 500);
-			}
-		});
-	}, []);
+  useEffect(() => {
+    authenticate().then(() => {
+      const ele = document.getElementById("ipl-progress-indicator");
+      if (ele) {
+        // fade out
+        ele.classList.add("available");
+        setTimeout(() => {
+          // remove from DOM
+          ele.outerHTML = "";
+        }, 500);
+      }
+    });
+  }, []);
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<Navbar />
-			</header>
-			{/* Provides the input for finding a location's forecast
-			 */}
-			<div className="App-body">
-				<Location />
-				{/* Displays the forecast results for the given location
-				 */}
-				<Forecast />
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar />
+      </header>
+      {/* Provides the input for finding a location's forecast
+       */}
+      <div className="App-body">
+        <Location />
+        {/* Displays the forecast results for the given location
+         */}
+        <Forecast />
+      </div>
+    </div>
+  );
 }
 
 export default App;
