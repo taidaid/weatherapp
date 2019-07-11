032a463439aec5eb2f66795868d8587ae1737293
Bryan <windsor.bryan@gmail.com>
2019-07-11 23:02:50 +0200

    added box styling

--- src/components/Currently/Currently.js
+++ src/components/Currently/Currently.js
@@ -24,20 +24,25 @@ const Currently = ({ forecast, getIcon }) => {
 
       <SmoothCollapse expanded={currentlyExpanded}>
         <div className="currently-description">
-          <div className="descriptor">Temperature: {forecast.temperature}</div>
-          <div className="descriptor">Humidity: {forecast.humidity}</div>
           <div className="descriptor">
-            Chance of Precipitation: {forecast.precipProbabidivty}
+            Temperature: {forecast.temperature}°F
           </div>
+          <div className="descriptor">Humidity: {forecast.humidity * 100}%</div>
           <div className="descriptor">
-            Precipitation Type: {forecast.precipType}
+            Chance of Precipitation: {forecast.precipProbability * 100}%
           </div>
+
+          {forecast.precipType ? (
+            <div className="descriptor">
+              Precipitation Type: {forecast.precipType}
+            </div>
+          ) : null}
 
           <div className="descriptor">
             Wind Direction: {forecast.windDirection}
           </div>
-          <div className="descriptor">Wind Gust: {forecast.windGust}</div>
-          <div className="descriptor">Wind Speed: {forecast.windSpeed}</div>
+          <div className="descriptor">Wind Gust: {forecast.windGust}mph</div>
+          <div className="descriptor">Wind Speed: {forecast.windSpeed}mph</div>
         </div>
       </SmoothCollapse>
     </div>
