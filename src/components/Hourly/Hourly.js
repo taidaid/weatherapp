54142ca7fe01f1ea75b6dd3ef81f1a7a7275742b
Bryan <windsor.bryan@gmail.com>
2019-07-11 23:59:49 +0200

    added hourly section with style and custom scroll

---
+++ src/components/Hourly/Hourly.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./Hourly.css";
+import SmoothCollapse from "react-smooth-collapse";
+
+const Hourly = ({ forecast, getIcon }) => {
+  const [hourlyExpanded, setHourlyExpanded] = useState(false);
+
+  const getHourFromUnixTime = unix_timestamp => {
+    const date = new Date(unix_timestamp * 1000);
+    const hours = date.getHours();
+    const minutes = "0" + date.getMinutes();
+    const seconds = "0" + date.getSeconds();
+    const formattedTime =
+      hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
+    return formattedTime;
+  };
+
+  const hourlyForecast = forecast.data.map((hour, index) => {
+    return (
+      <div className="hourly-description" key={`${index}`}>
+        <h4 className="date-time">{getHourFromUnixTime(hour.time)}</h4>
+        <div className="hourly-precip-chance">
+          {getIcon("rain")}
+          <div className="day-precip-chance-text  ">
+            {Math.floor(hour.precipProbability * 100)}%
+          </div>
+        </div>
+        <p className="hourly-summary">{hour.summary}</p>
+        <p className="hourly-temp">Temp.: {Math.floor(hour.temperature)}°F</p>
+      </div>
+    );
+  });
+
+  return (
+    <div className="hourly">
+      <div
+        className="hourly-section-title"
+        onClick={() => setHourlyExpanded(!hourlyExpanded)}
+      >
+        <h3>{hourlyExpanded ? "-" : "+"}Hourly</h3>
+        {<div>{getIcon(forecast.icon)}</div>}
+      </div>
+      <h6 className="summary">({forecast.summary})</h6>
+      <SmoothCollapse expanded={hourlyExpanded}>
+        <div className="hourly-forecasts">{hourlyForecast}</div>
+      </SmoothCollapse>
+    </div>
+  );
+};
+
+export default Hourly;
